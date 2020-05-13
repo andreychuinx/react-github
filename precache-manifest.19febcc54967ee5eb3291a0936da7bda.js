a08b13e79e0ea2f5d2126b6ac33bf82b68272180
andrey amrulloh <muhamad.amrulloh@bizzy.co.id>
2020-05-13 23:11:50 +0700

    Updates

---
+++ precache-manifest.19febcc54967ee5eb3291a0936da7bda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405bf39c82bb85a5683bb4d34d333b25",
+    "url": "/react-github/index.html"
+  },
+  {
+    "revision": "7d153ae3007a6a74bb0c",
+    "url": "/react-github/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "79e306f3d0f87e77a035",
+    "url": "/react-github/static/js/2.729cbb42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github/static/js/2.729cbb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d153ae3007a6a74bb0c",
+    "url": "/react-github/static/js/main.bd209685.chunk.js"
+  },
+  {
+    "revision": "05bf5e3a03bc779e17c3",
+    "url": "/react-github/static/js/runtime-main.3dd52def.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-github/static/media/logo.5d5d9eef.svg"
+  }
+]);
